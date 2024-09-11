d2728b340365abf68f749c65450a7c538036877a
Gulab kumar <kumargulab500@gmail.com>
2024-09-11 09:17:41 -0700

    Update settings.py

--- food/settings.py
+++ food/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-rfy*8o$nr$nzu$b(b%h7zmo&=+zr)vnyver#0#%)3^vwta54%(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
